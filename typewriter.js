6c93871bf9d4ba3c25de39ae7ae18e4cd802f986
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2023-02-17 02:36:21 -0500

    added code comments

--- typewriter.js
+++ typewriter.js
@@ -1,16 +1,18 @@
 
 const printSentance = function(sentance) {
+  // Loop over each letter in the sentance
   for (let i = 0; i < sentance.length; i++) {
+    // Use the timer to delay each letter
     setTimeout(() => {
       // prints each letter on same line:
       process.stdout.write(sentance[i]);
+      // increase the delay on each letter, * by the current index
     }, 50 * i);
     setTimeout(() => {
+      // Create a new line after the letters finish printing
       console.log();
     }, 50 * sentance.length);
   }
 };
 
 printSentance("print it");
-
-
