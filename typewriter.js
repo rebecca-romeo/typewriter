297220faa3a01e34c7cd963b1462ad85f0287697
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2023-02-17 02:31:09 -0500

    refactored to set delay on each letter typed

--- typewriter.js
+++ typewriter.js
@@ -1,10 +1,13 @@
 
+const printSentance = function(sentance) {
+  for (let i = 0; i < sentance.length; i++) {
+    setTimeout(() => {
+      // prints each letter on same line:
+      process.stdout.write(sentance[i]);
+    }, 50 * i);
+  }
+};
 
-const sentence = "hello there";
-for (const char of sentence) {
-  setTimeout(() => {
-    // prints each letter in the same line:
-    process.stdout.write(char);
-  }, 1000)
-}
+printSentance("print it");
+
 
